3d3acd436ed71aabe206889299cdc12684a05f6d
Faizan Ali <faizan.abdulali@koderlabs.com>
2024-09-08 16:11:30 +0500

    Users REST API completed.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,34 +4,44 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
+
+import { UsersService } from './users.service';
+import { Prisma } from '@prisma/client';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: Prisma.UserCreateInput) {
+    return this.userService.create(createUserDto);
+  }
+
   @Get()
-  findAll() {
-    return [];
+  findAll(@Query('role') role?: 'BUYER' | 'SELLER' | 'ADMIN') {
+    return this.userService.findAll(role);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return { id };
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.findOne(id);
   }
 
-  @Post()
-  create(@Body() user: {}) {
-    return user;
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: Prisma.UserUpdateInput,
+  ) {
+    return this.userService.update(id, updateUserDto);
   }
 
-  @Patch('id')
-  update(@Param('id') id: string, @Body() userUpdate: {}) {
-    return { id, ...userUpdate };
-  }
-
-  @Delete('id')
-  delete(@Param('id') id: string) {
-    return { id };
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.remove(id);
   }
 }
